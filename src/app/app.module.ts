ec1d61296ea6459f3c57c6cf0e99a79733be7240
mhazak <hazak.marek@gmail.com>
2022-07-05 15:02:39 +0200

    [module] http client module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { CreateAccountComponent } from './pages/create-account/create-account.co
     AppRoutingModule,
     BrowserAnimationsModule,
 	ReactiveFormsModule,
-	MaterialModule
+	MaterialModule,
+	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
