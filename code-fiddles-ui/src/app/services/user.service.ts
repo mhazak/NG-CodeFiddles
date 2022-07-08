baac9cfcf0899c977fe0a713c809359820e32274
mhazak <hazak.marek@gmail.com>
2022-07-08 12:19:41 +0200

    [dev] initAuthListener

--- code-fiddles-ui/src/app/services/user.service.ts
+++ code-fiddles-ui/src/app/services/user.service.ts
@@ -1,15 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 import { environment } from 'src/environments/environment';
 import { User } from './user.model';
+import * as fromRoot from '../app.reducer'
+import * as userActions from './user.actions';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  	constructor(private http: HttpClient) { }
+  	constructor(private http: HttpClient, private store: Store<fromRoot.State>, private router: Router) { }
 
 	createAccount (model: User) {
 		this.http.post(environment.backend + '/api/users/create/', model).subscribe(res => {
@@ -21,7 +25,10 @@ export class UserService {
 
 	login (model: User) {
 
-		this.http.post(environment.backend + '/api/users/login/', model).subscribe(res => {
+		this.http.post(environment.backend + '/api/users/login/', model).subscribe((res: any) => {
+			if (res.success) {
+				this.store.dispatch(new userActions.SetAuthenticated());
+			}
 			console.log({res});
 		}, err => {
 			console.log({err});
@@ -30,6 +37,14 @@ export class UserService {
 	}
 
 	initAuthListener () {
-
+		const isAuth = localStorage.getItem('isAuth');
+		if (isAuth) {
+			this.store.dispatch(new userActions.SetAuthenticated());
+			this.router.navigate(['/']);
+		} else {
+			this.store.dispatch(new userActions.SetUnauthenticated());
+			this.router.navigate(['/login']);
+		}
+		console.log({isAuth});
 	}
 }
