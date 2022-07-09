d29b175b2a97506d34fb7dd69e31ce21fbfcf334
mhazak <hazak.marek@gmail.com>
2022-07-09 19:47:21 +0200

    [dev] coding component route

--- code-fiddles-ui/src/app/app-routing.module.ts
+++ code-fiddles-ui/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CodingComponent } from './pages/coding/coding.component';
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,7 +9,8 @@ import { UserGuard } from './services/user.guard';
 const routes: Routes = [
 	{ path: '', component: HomeComponent, canActivate: [UserGuard]},
 	{ path: 'login', component: LoginComponent },
-	{ path: 'create-account', component: CreateAccountComponent }
+	{ path: 'create-account', component: CreateAccountComponent },
+	{ path: 'coding/:fiddleid', component: CodingComponent }
 ];
 
 @NgModule({
