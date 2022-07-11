9100890bfc202f4730aab6b548e529b150ed2be6
mhazak <hazak.marek@gmail.com>
2022-07-11 20:53:46 +0200

    [dev] new ngrx syntax

---
+++ code-fiddles-ui/src/app/services/fiddle/fiddle.actions.ts
@@ -0,0 +1,18 @@
+import { createAction, props } from "@ngrx/store";
+import { FiddleModel } from "./fiddle.model";
+
+export const LOAD_FIDDLES = createAction('[Fiddles] Get available fiddles loading');
+export const LOAD_FIDDLES_SUCCESS = createAction('[Fiddles] Get available fiddles success', props<{fiddles: FiddleModel[]}>());
+export const LOAD_FIDDLES_FAIL = createAction('[Fiddles] Get available fiddles fail', props<{message: string}>());
+
+export const FIDDLE_ADDED = createAction('[Fiddles] New fiddle is creating', props<{fiddle: FiddleModel}>());
+export const FIDDLE_ADDED_SUCCESS = createAction('[Fiddles] New fiddle was created successfully');
+export const FIDDLE_ADDED_FAIL = createAction('[Fiddles] New fiddle was not created, error', props<{message: string}>());
+
+export const FIDDLE_UPDATED = createAction('[Fiddles] Fiddle is updating');
+export const FIDDLE_UPDATED_SUCCESS = createAction('[Fiddles] Fiddle was updated successfully');
+export const FIDDLE_UPDATED_FAIL = createAction('[Fiddles] Fiddle was not updated, error');
+
+export const FIDDLE_REMOVED = createAction('[Fiddles] Fiddle is removing');
+export const FIDDLE_REMOVED_SUCCESS = createAction('[Fiddles] Fiddle was removed successfully');
+export const FIDDLE_REMOVED_FAIL = createAction('[Fiddles] Fiddle was not removed, error');
