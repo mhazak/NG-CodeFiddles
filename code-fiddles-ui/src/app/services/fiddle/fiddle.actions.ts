882c07361aa1fea491ca0f27be63ed93d7f23e35
mhazak <hazak.marek@gmail.com>
2022-07-12 07:43:54 +0200

    [dev] property names changed

--- code-fiddles-ui/src/app/services/fiddle/fiddle.actions.ts
+++ code-fiddles-ui/src/app/services/fiddle/fiddle.actions.ts
@@ -1,13 +1,13 @@
 import { createAction, props } from "@ngrx/store";
 import { FiddleModel } from "./fiddle.model";
 
-export const LOAD_FIDDLES = createAction('[Fiddles] Get available fiddles loading');
+export const LOAD_FIDDLES = createAction('[Fiddles] Get available fiddles loading', props<{loading: boolean}>());
 export const LOAD_FIDDLES_SUCCESS = createAction('[Fiddles] Get available fiddles success', props<{fiddles: FiddleModel[]}>());
-export const LOAD_FIDDLES_FAIL = createAction('[Fiddles] Get available fiddles fail', props<{message: string}>());
+export const LOAD_FIDDLES_FAIL = createAction('[Fiddles] Get available fiddles fail', props<{error: string}>());
 
 export const FIDDLE_ADDED = createAction('[Fiddles] New fiddle is creating', props<{fiddle: FiddleModel}>());
 export const FIDDLE_ADDED_SUCCESS = createAction('[Fiddles] New fiddle was created successfully');
-export const FIDDLE_ADDED_FAIL = createAction('[Fiddles] New fiddle was not created, error', props<{message: string}>());
+export const FIDDLE_ADDED_FAIL = createAction('[Fiddles] New fiddle was not created, error', props<{error: string}>());
 
 export const FIDDLE_UPDATED = createAction('[Fiddles] Fiddle is updating');
 export const FIDDLE_UPDATED_SUCCESS = createAction('[Fiddles] Fiddle was updated successfully');
