5a75151b6697c1e2f16a4ba220253923a05ff403
mhazak <hazak.marek@gmail.com>
2022-07-12 07:43:16 +0200

    [dev] fiddle separated component

---
+++ code-fiddles-ui/src/app/pages/home/fiddle/fiddle.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FiddleComponent } from './fiddle.component';
+
+describe('FiddleComponent', () => {
+  let component: FiddleComponent;
+  let fixture: ComponentFixture<FiddleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FiddleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FiddleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
