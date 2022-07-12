8f5be42f9c7a45edf75e08cc04387fe4f35de5c0
mhazak <hazak.marek@gmail.com>
2022-07-12 07:44:34 +0200

    [dev] read and create reducers

--- code-fiddles-ui/src/app/services/fiddle/fiddle.reducer.ts
+++ code-fiddles-ui/src/app/services/fiddle/fiddle.reducer.ts
@@ -6,21 +6,57 @@ export interface State {
 	availableFiddles: FiddleModel[],
 	addedFiddle: FiddleModel,
 	updatingFiddleId: string,
-	error: string
+	error: string,
+	loading: boolean
 }
 
 const initialState: State = {
 	availableFiddles: null,
 	addedFiddle: null,
 	updatingFiddleId: '',
-	error: ''
+	error: '',
+	loading: false
 };
 
 export const fiddleReducer = createReducer(initialState,
+	on(FiddleActions.LOAD_FIDDLES, (state, { loading }) => {
+		return {
+			...state,
+			loading: loading
+		}
+	}),
 	on(FiddleActions.LOAD_FIDDLES_SUCCESS, (state, { fiddles } ) => {
 		return {
 			...state,
 			availableFiddles: fiddles
+		}
+	}),
+	on(FiddleActions.LOAD_FIDDLES_FAIL, (state, { error }) => {
+		return {
+			...state,
+			error: error
+		}
+	}),
+	on(FiddleActions.FIDDLE_ADDED, (state, { fiddle }) => {
+		return {
+			...state,
+			addedFiddle: fiddle,
+			loading: true
+		}
+	}),
+	on(FiddleActions.FIDDLE_ADDED_SUCCESS, (state) => {
+		return {
+			...state,
+			addedFiddle: null,
+			loading: false
+		}
+	}),
+	on(FiddleActions.FIDDLE_ADDED_FAIL, (state, { error }) => {
+		return {
+			...state,
+			addedFiddle: null,
+			loading: false,
+			error: error
 		}
 	})
 	// on(FiddleActions.FIDDLE_ADDED_SUCCESS, (state) => state.av)
