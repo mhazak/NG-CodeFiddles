7d98734de076b290fcf433cc80cc73cc7701e3a4
mhazak <hazak.marek@gmail.com>
2022-07-12 20:56:45 +0200

    [dev] new login with ngrx

--- code-fiddles-ui/src/app/pages/login/login.component.ts
+++ code-fiddles-ui/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserService } from 'src/app/services/user.service';
+import { Store } from '@ngrx/store';
+import { SET_AUTH } from 'src/app/services/user.actions';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class LoginComponent implements OnInit {
 
-	constructor(private userService: UserService) { }
+	constructor(private store: Store) { }
 
 	loginForm = new FormGroup({
 		email: new FormControl('', [Validators.required, Validators.email]),
@@ -20,9 +21,9 @@ export class LoginComponent implements OnInit {
   	}
 
 	onLogin() {
-		this.userService.login({
+		this.store.dispatch(SET_AUTH({user: {
 			email: this.loginForm.value['email'],
 			password: this.loginForm.value['password']
-		})
+		}}))
 	}
 }
