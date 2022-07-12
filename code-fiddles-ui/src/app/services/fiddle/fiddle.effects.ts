17db7dbfee8460421255c77afc157f60a7694c71
mhazak <hazak.marek@gmail.com>
2022-07-12 20:58:18 +0200

    [dev] add fiddle effect

--- code-fiddles-ui/src/app/services/fiddle/fiddle.effects.ts
+++ code-fiddles-ui/src/app/services/fiddle/fiddle.effects.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { EMPTY, of } from "rxjs";
-import { catchError, map, mergeMap } from "rxjs/operators";
-import { LOAD_FIDDLES, LOAD_FIDDLES_FAIL, LOAD_FIDDLES_SUCCESS } from "./fiddle.actions";
+import { of } from "rxjs";
+import { catchError, exhaustMap, map, mergeMap } from "rxjs/operators";
+import { UserService } from "../user.service";
+import { FIDDLE_ADDED, FIDDLE_ADDED_FAIL, FIDDLE_ADDED_SUCCESS, LOAD_FIDDLES, LOAD_FIDDLES_FAIL, LOAD_FIDDLES_SUCCESS } from "./fiddle.actions";
 import { FiddleModel } from "./fiddle.model";
 import { FiddleService } from "./fiddle.service";
 
 @Injectable()
 export class FiddleEffects {
 
-	constructor(private actions$: Actions, private fiddleService: FiddleService) {}
+	constructor(private actions$: Actions, private fiddleService: FiddleService, private userService: UserService) {}
 
 	loadFiddles$ = createEffect(() =>
 		this.actions$.pipe(
@@ -20,6 +21,18 @@ export class FiddleEffects {
 					catchError((err) => (of(LOAD_FIDDLES_FAIL({ error: err }))))
 				)
 			)
+		)
+	);
+
+	addFiddle$ = createEffect(() =>
+
+		this.actions$.pipe(
+			ofType(FIDDLE_ADDED),
+			exhaustMap((action) => this.fiddleService.createFiddle(action.fiddle)
+				.pipe(
+					map(response => FIDDLE_ADDED_SUCCESS()),
+					catchError(err => of(FIDDLE_ADDED_FAIL({error: err})))
+				))
 		)
 	);
 
