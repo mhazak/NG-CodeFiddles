d08d674106ff4e5a3fc51f1d0250ce0c0ccdaf58
mhazak <hazak.marek@gmail.com>
2022-07-12 20:56:24 +0200

    [dev] action calling via ngrx

--- code-fiddles-ui/src/app/pages/home/home.component.ts
+++ code-fiddles-ui/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { LOAD_FIDDLES } from 'src/app/services/fiddle/fiddle.actions';
+import { FIDDLE_ADDED, LOAD_FIDDLES } from 'src/app/services/fiddle/fiddle.actions';
 import { FiddleModel } from 'src/app/services/fiddle/fiddle.model';
 
 @Component({
@@ -14,6 +14,10 @@ export class HomeComponent implements OnInit {
   	constructor(private store: Store<{fiddles: FiddleModel[]}>) { }
 
 	fiddles$: Observable<FiddleModel[]> = this.store.select(state => state.fiddles);
+
+	onSubmit() {
+		this.store.dispatch(FIDDLE_ADDED({ fiddle: { name: 'new', language: 'JavaScript', code: 'console.log(\"Hello World!\");' }}))
+	}
 	ngOnInit(): void {
 		this.store.dispatch(LOAD_FIDDLES({ loading: true }))
 	}
