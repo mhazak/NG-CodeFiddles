7c48376ab2304c7a8b83187b594273a9eabbdc48
mhazak <hazak.marek@gmail.com>
2022-07-12 20:57:31 +0200

    [dev] new user reducer

--- code-fiddles-ui/src/app/services/user.reducer.ts
+++ code-fiddles-ui/src/app/services/user.reducer.ts
@@ -1,32 +1,115 @@
-import { AuthActions, CHECK_AUTH, SET_AUTH, SET_NON_AUTH } from "./user.actions";
+import { createReducer, on } from "@ngrx/store";
+import * as UserActions from "./user.actions";
 
 export interface State {
 	isAuthenticated: boolean;
+	loading: boolean;
+	error: string;
+	authenticatedUser: { id: string, email: string };
 }
 
-const initialState: State = { isAuthenticated: false };
+const initialState: State = { isAuthenticated: false, loading: true, error: '', authenticatedUser: null };
 
-export function authReducer (state: State = initialState, action: AuthActions) {
-	switch (action.type) {
+export const authReducer = createReducer(initialState,
+	on(UserActions.SET_NON_AUTH, (state) => {
+		return {
+			... state,
+			loading: true
+		}
+	}),
+	on(UserActions.SET_NON_AUTH_SUCCESS, (state) => {
+		return {
+			... state,
+			loading: false,
+			isAuthenticated: false
+		}
+	}),
+	on(UserActions.SET_NON_AUTH_FAILED, (state, { error }) => {
+		return {
+			... state,
+			loading: false,
+			isAuthenticated: true,
+			error: error
+		}
+	}),
+	on(UserActions.SET_AUTH, (state) => {
+		return {
+			... state,
+			loading: true,
+			isAuthenticated: false,
+		}
+	}),
 
-		case SET_AUTH:
-			localStorage.setItem('isAuth', 'true');
-			return { isAuthenticated: true };
+	on(UserActions.SET_AUTH_SUCCESS, (state, { authenticatedUser }) => {
+		return {
+			... state,
+			loading: false,
+			isAuthenticated: true,
+			authenticatedUser: authenticatedUser
+		}
+	}),
 
-		case SET_NON_AUTH:
-			localStorage.removeItem('isAuth');
-			return { isAuthenticated: false };
+	on(UserActions.SET_AUTH_FAILED, (state, { error }) => {
+		return {
+			... state,
+			loading: false,
+			isAuthenticated: false,
+			error: error
+		}
+	}),
 
-		case CHECK_AUTH:
-			const _isAuth = localStorage.getItem('isAuth');
-			if (_isAuth)
-				return { isAuthenticated: true };
-			else
-				return { isAuthenticated: false };
+	on(UserActions.GET_USER, (state) => {
+		return {
+			...state,
+			loading: true
+		}
+	}),
 
-		default:
-			return state;
-	}
-}
+	on(UserActions.GET_USER_SUCCESS, (state) => {
+		return {
+			...state,
+			loading: false,
+			user: state.authenticatedUser
+		}
+	}),
 
-export const getIsAuth = (state: State) => state.isAuthenticated;
+	on(UserActions.GET_USER_FAILED, (state, { error }) => {
+		return {
+			...state,
+			loading: false,
+			message: error
+		}
+	}),
+
+	on(UserActions.CHECK_AUTH, (state) => {
+		return {
+			...state,
+			isAuthenticated: state.isAuthenticated
+		}
+	}),
+
+)
+// export function authReducer (state: State = initialState, action: AuthActions) {
+// 	switch (action.type) {
+
+// 		case SET_AUTH:
+// 			localStorage.setItem('isAuth', 'true');
+// 			return { isAuthenticated: true };
+
+// 		case SET_NON_AUTH:
+// 			localStorage.removeItem('isAuth');
+// 			return { isAuthenticated: false };
+
+// 		case CHECK_AUTH:
+// 			const _isAuth = localStorage.getItem('isAuth');
+// 			if (_isAuth)
+// 				return { isAuthenticated: true };
+// 			else
+// 				return { isAuthenticated: false };
+
+// 		default:
+// 			return state;
+// 	}
+// }
+
+// export const getIsAuth = (state: State) => state.isAuthenticated;
